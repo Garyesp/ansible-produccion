290f08f6c0b32e43e15e2887141592a3cb8fa9c9
Gary Espinoza <tec.gespinoza@gmail.com>
2026-02-11 14:18:34 +0000

    cambiando mensaje de examen

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-app.get("/", (req, res) => res.send("OK - DDDDespliegue Ansible :: oooook Gary Espinoza Villagomez\n"));
+app.get("/", (req, res) => res.send("Examen DAW Despliegue inicial\n"));
 app.listen(port, () => console.log(`Listening on ${port}`));
